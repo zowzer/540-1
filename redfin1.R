853259144fe82e31ee645645d5d91f9f84d1401f
Zowzer <55644860+zowzer@users.noreply.github.com>
2019-10-01 22:50:57 -0700

    Update redfin1.R
    Normalize function added and compared to existing plots with minimal to no differences.

--- redfin1.R
+++ redfin1.R
@@ -14,8 +14,6 @@ redfin1 <- read_excel("~/MSBA/MSBA Git/540-1/redfin_2019-09-24-18-15-38.xlsm", s
 
 view(redfin1)
 
-summary(redfin1)
-
 summary_redfin1 <- summary(redfin1)
 
 price <-(redfin1$`PRICE`)
@@ -25,6 +23,13 @@ sq_ft <-(redfin1$`SQUARE FEET`)
 lot_size <-(redfin1$`LOT SIZE`)
 yr_built <-(redfin1$`YEAR BUILT`)
 
+#Normalize function
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+#Generating and adding normalized yr_built to data frame
+redfin1$yr_built_norm<-normalize(redfin1$'YEAR BUILT')
 
 #Scatter Plot of  beds, baths, sq_ft, lot_size, and yr_built by price
 ggplot(data=redfin1) + geom_point(mapping=aes(x=beds, y=price))
@@ -32,6 +37,7 @@ ggplot(data=redfin1) + geom_point(mapping=aes(x=baths, y=price))
 ggplot(data=redfin1) + geom_point(mapping=aes(x=sq_ft, y=price))
 ggplot(data=redfin1) + geom_point(mapping=aes(x=lot_size, y=price))
 ggplot(data=redfin1) + geom_point(mapping=aes(x=yr_built, y=price))
+ggplot(data=redfin1) + geom_point(mapping=aes(x=yr_built_norm, y=price)) #Normalized yr_built
 
 #Scatter plot of beds, baths, sq_ft, lot_size, and yr_built by price with smoothing
 qplot(beds, price, data = redfin1, geom = c('point','smooth'))
@@ -39,6 +45,7 @@ qplot(baths, price, data = redfin1, geom = c('point','smooth'))
 qplot(sq_ft, price, data = redfin1, geom = c('point','smooth'))
 qplot(lot_size, price, data = redfin1, geom = c('point','smooth'))
 qplot(yr_built, price, data = redfin1, geom = c('point','smooth'))
+qplot(yr_built_norm, price, data = redfin1, geom = c('point','smooth')) #Normalized yr_built
 
 
 #Histograms of beds, baths, sq_ft, lot_size, and yr_built
@@ -48,4 +55,4 @@ ggplot(data=redfin1, aes(baths)) + geom_histogram(bins = 10, binwidth = .5)
 ggplot(data=redfin1, aes(sq_ft)) + geom_histogram(bins = 6, binwidth = 1000)
 ggplot(data=redfin1, aes(lot_size)) + geom_histogram()
 ggplot(data=redfin1, aes(yr_built)) + geom_histogram(bins = 24, binwidth = 5)
-
+ggplot(data=redfin1, aes(yr_built_norm)) + geom_histogram() #Normalized yr_built
