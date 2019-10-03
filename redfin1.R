65ebf06eeb400ac1a58414db3892bb1004d1d1b5
Zowzer <55644860+zowzer@users.noreply.github.com>
2019-10-03 00:30:22 -0700

    Update redfin1.R
    Added code for evaluating correlation, the 2x ways of generating the OLS plots (cut/paste and via for loop <- preferred), and added OLS model (could be improved with a for loop as well).

--- redfin1.R
+++ redfin1.R
@@ -1,5 +1,6 @@
 install.packages("XLConnect") #installs package to read Excel spreadsheets
 install.packages("tidyverse")
+install.packages("ggplot2")
 
 
 getwd()
@@ -56,3 +57,47 @@ ggplot(data=redfin1, aes(sq_ft)) + geom_histogram(bins = 6, binwidth = 1000)
 ggplot(data=redfin1, aes(lot_size)) + geom_histogram()
 ggplot(data=redfin1, aes(yr_built)) + geom_histogram(bins = 24, binwidth = 5)
 ggplot(data=redfin1, aes(yr_built_norm)) + geom_histogram() #Normalized yr_built
+
+#Correlation evals
+cor(beds, price)
+cor(baths, price)
+cor(sq_ft, price) #probably highest correlation
+cor(lot_size, price)
+cor(yr_built, price)
+cor(redfin1$yr_built_norm, price) #Same as yr_built
+
+# OLS plots (once loop between lines 87-97 is validated, this section can be deleted)
+redfin1 %>%
+  ggplot(aes(x = sqrt(beds), y = sqrt(price))) +
+  geom_point(colour = "red") +
+  geom_smooth(method = "lm", fill = NA) 
+redfin1 %>%
+  ggplot(aes(x = sqrt(sq_ft), y = sqrt(price))) +
+  geom_point(colour = "red") +
+  geom_smooth(method = "lm", fill = NA)
+redfin1 %>%
+  ggplot(aes(x = sqrt(lot_size), y = sqrt(price))) +
+  geom_point(colour = "red") +
+  geom_smooth(method = "lm", fill = NA)
+redfin1 %>%
+  ggplot(aes(x = sqrt(yr_built), y = sqrt(price))) +
+  geom_point(colour = "red") +
+  geom_smooth(method = "lm", fill = NA)
+
+# OLS plots via for loop (verify accuracy by checking against individual plots between lines 69-85)
+Y <- sqrt(price)
+df <- data.frame(A = sqrt(sq_ft), B = sqrt(lot_size), C = sqrt(yr_built), Y = Y)
+colNames <- names(df)[1:3]
+for(i in colNames){
+  plt <- ggplot(df, aes_string(x=i, y = Y)) +
+    geom_point(color="red") +
+    geom_smooth(method=lm, fill=NA)
+  print(plt)
+  Sys.sleep(2)
+}
+
+
+#OLS model
+lmodel <- lm(sqrt(price) ~ sqrt(yr_built), data = redfin1)
+lmodel$coefficients
+summary(lmodel) #statistical info generated here
